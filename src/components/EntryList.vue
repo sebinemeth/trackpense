9ff88bfcadfa5f4653bb78219ae171e683865184
Sebi Nemeth <sebi.nemeth@apicore.de>
2020-12-28 12:50:47 +0100

    init

---
+++ src/components/EntryList.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <b-list-group>
+      <b-list-group-item v-for="e in listitems" v-bind:key="e.id">
+        {{ e.descripiton }}
+      </b-list-group-item>
+    </b-list-group>
+  </div>
+</template>
