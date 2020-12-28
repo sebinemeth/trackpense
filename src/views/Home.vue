e855f6ae09858e61fcafeef7ef6dc6d3da413ec4
Sebi Nemeth <sebi.nemeth@apicore.de>
2020-12-28 15:27:33 +0100

    chart init

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -25,15 +25,19 @@
         </b-media>
       </b-card>
       <h4>Balance</h4>
-      <b-card class="my-3"> </b-card>
+      <b-card class="my-3">
+        <line-chart :chart-data="chartData"></line-chart>
+      </b-card>
 
       <h4>Recent activity</h4>
 
       <div class="d-flex justify-content-between">
-        <b-button v-b-modal.addExpense
-          ><b-icon-file-minus /> Add expense</b-button
-        >
-        <b-button v-b-modal.addIncome><b-icon-file-plus /> Add income</b-button>
+        <b-button v-b-modal.addExpense>
+          <b-icon-file-minus /> Add expense
+        </b-button>
+        <b-button v-b-modal.addIncome>
+          <b-icon-file-plus /> Add income
+        </b-button>
       </div>
 
       <b-modal id="addExpense" title="Add expense" hide-footer>
@@ -53,23 +57,45 @@
 import firebase from "firebase";
 import AddEntry from "@/components/AddEntry.vue";
 import EntryList from "@/components/EntryList.vue";
+import LineChart from "@/components/LineChart.js";
 
 export default {
   name: "Home",
-  components: { AddEntry, EntryList },
+  components: { AddEntry, EntryList, LineChart },
   props: ["user"],
   data() {
     return { listitems: [] };
   },
+  computed: {
+    chartData() {
+      return {
+        labels: this.listitems.map((item) =>
+          new Date(item.timestamp).toLocaleString()
+        ),
+        datasets: [
+          {
+            label: "Data One",
+            backgroundColor: "#f87979",
+            data: this.listitems.map(
+              (item) => item.amount * (item.income ? 1 : -1)
+            ),
+          },
+        ],
+      };
+    },
+  },
   mounted() {
     const database = firebase.database();
+
+    console.log(this.user.uid);
 
     const ref = database.ref("users/" + this.user.uid + "/transactions");
     ref.on("value", (snapshot) => {
       const data = snapshot.val();
-      this.listitems = Object.entries(data).map((entry) =>
-        Object.assign(entry[1], { id: entry[0] })
-      );
+      if(snapshot)
+        this.listitems = Object.entries(data).map((entry) =>
+          Object.assign(entry[1], { id: entry[0] })
+        );
     });
   },
 };
