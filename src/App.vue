39763f2151916f409c903f803d7686d34c393e64
Sebi Nemeth <sebi.nemeth@apicore.de>
2020-12-28 12:41:34 +0100

    login & signup redesign

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,12 @@
 <template>
   <div id="app">
     <div id="nav">
-      <h3>trackpense</h3>
+      <b-navbar toggleable="lg" type="dark" variant="dark">
+        <b-navbar-brand href="#">TrackPense</b-navbar-brand>
+      </b-navbar>
     </div>
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
+<style lang="scss"></style>
