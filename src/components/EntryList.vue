4675d1ddb54e9a47dc33306e8cadf63b85370f5a
lebma <marci.lebek@gmail.com>
2020-12-28 13:59:03 +0100

    listitems added to EntryList

--- src/components/EntryList.vue
+++ src/components/EntryList.vue
@@ -2,8 +2,22 @@
   <div>
     <b-list-group>
       <b-list-group-item v-for="e in listitems" v-bind:key="e.id">
-        {{ e.descripiton }}
+        <div>{{ e.descripiton }}</div>
+        <div>{{ e.date }}</div>
+        <div float="right">{{ e.amount }}</div>
       </b-list-group-item>
     </b-list-group>
   </div>
 </template>
+
+<script>
+export default {
+  name: "Entry List",
+  props: {
+    listitems: {
+      type: Array,
+      default: []
+    }
+  }
+}
+</script>
