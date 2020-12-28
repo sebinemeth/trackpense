9f323caa12335ff810a0d8a1d3b003526648ecf1
lebma <marci.lebek@gmail.com>
2020-12-28 17:09:56 +0100

    entrylist design update

--- src/components/EntryList.vue
+++ src/components/EntryList.vue
@@ -4,11 +4,11 @@
       <b-list-group-item v-for="e in listitems" v-bind:key="e.id">
         <div class="d-flex w-100 justify-content-between">
           <h5>{{ e.description }}</h5>
-          <div>{{ new Date().toLocaleDateString(e.timestamp) }}</div>
+          <h6>{{ new Date().toLocaleDateString(e.timestamp) }}</h6>
         </div>
         <div class="d-flex w-100 justify-content-between">
-          <div v-if="e.income" style="color: green">{{ e.amount }}</div>
-          <div v-else style="color: red">{{ -e.amount }}</div>
+          <h5 v-if="e.income" style="color: green">{{ e.amount }}</h5>
+          <h5 v-else style="color: red">{{ -e.amount }}</h5>
           <b-button variant="outline-secondary"> Edit </b-button>
         </div>
       </b-list-group-item>
